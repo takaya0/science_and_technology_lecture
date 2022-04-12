4e9e0556f49476a8a4e6aa2e29a6158795bf94f0
takaya0 <ytk.koizumi@gmail.com>
2022-04-13 01:33:45 +0900

    update slides

--- src/quadratic.py
+++ src/quadratic.py
@@ -5,7 +5,11 @@ from pymanopt.solvers import SteepestDescent
 from matplotlib import pyplot as plt
 import autograd.numpy as np
 
+import time
+
 from utils import get_random_sym_matrix
+
+image_path = '../Images/'
 
 
 def RayleighQuotientsOptimization(N):
@@ -16,19 +20,26 @@ def RayleighQuotientsOptimization(N):
         retval = np.dot(x.T, np.dot(A, x))
         return retval
     problem = Problem(Sphere(N), cost=cost)
+    now = time.perf_counter()
     solver = SteepestDescent(
-        maxiter=8000, logverbosity=2, mingradnorm=2.0 * 1e-1, maxtime=5000)
+        maxiter=8000, logverbosity=2, mingradnorm=0.001, maxtime=5000)
     Xopt = solver.solve(problem)
+
+    print(f'計算時間 : {time.perf_counter() - now }秒')
+
     losses = Xopt[1]['iterations']['f(x)']
     times = Xopt[1]['iterations']['time']
     return losses, times
 
 
 def main():
+    losses, times = RayleighQuotientsOptimization(N=5000)
+    index = np.arange(1, len(losses) + 1)
+    plt.plot(index, losses)
+    plt.xlabel('Iterations')
+    plt.ylabel('function value')
 
-    experiment1_losses = RayleighQuotientsOptimization(N=10)
-    experiment2_losses = RayleighQuotientsOptimization(N=100)
-    experiment3_losses = RayleighQuotientsOptimization(N=1000)
+    plt.savefig(image_path + 'quad_5000.png')
 
 
 if __name__ == "__main__":
